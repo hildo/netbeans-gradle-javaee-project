70c3ebc01fbe9c8592dfda214563ed574dc6cbf1
martin <martin@linux>
2013-11-20 04:26:53 +0100

    Creating basic implementations of Sources and ProjectWebRootProvider SPI, adding implementation dependency on web.common module, changing pom dependencies to be bounded to latest RELEASE74 version, changing the module to use Java 7 as it's standard and recommended JDK version since NetBeans 7.4

---
+++ netbeans-gradle-javaee-plugin/src/main/java/org/netbeans/gradle/javaee/web/utils/Constants.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.netbeans.gradle.javaee.web.utils;
+
+/**
+ * General constants which does not belong logically to any particular class.
+ * 
+ * <p>
+ * This class is <i>immutable</i> and thus <i>thread safe</i>.
+ *
+ * @author Martin Janicek <mjanicek@netbeans.org>
+ */
+public final class Constants {
+
+    private Constants() {
+    }
+
+    public static final String WEB_ROOT = "web_root"; // NOI18N
+}
